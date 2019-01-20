1053e907d0a41db1c30250a112fc720a50f0edf4
taomee <taomee517@qq.com>
2019-01-20 16:29:06 +0800

    加入回显案例

--- src/main/java/com/demo/netty/server/MyChannelHandler.java
+++ src/main/java/com/demo/netty/server/MyChannelHandler.java
@@ -1,11 +1,17 @@
 package com.demo.netty.server;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
 
 public class MyChannelHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        String data = buf.toString(Charset.forName("utf-8"));
+        System.out.println("收到消息：" + data);
         ctx.writeAndFlush(msg);
     }
 
