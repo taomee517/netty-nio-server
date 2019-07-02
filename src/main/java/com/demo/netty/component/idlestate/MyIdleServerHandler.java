d2abf1d2a5ce39d0699a7ff4dddedb94810e9d58
taomee517 <taomee517@qq.com>
2019-07-02 08:49:44 +0800

    最好的心跳回复，就是不回复！

--- src/main/java/com/demo/netty/component/idlestate/MyIdleServerHandler.java
+++ src/main/java/com/demo/netty/component/idlestate/MyIdleServerHandler.java
@@ -21,6 +21,11 @@ public class MyIdleServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         log.info("收到客户端消息：{}", msg);
+        /**通常服务端要对心跳包做出响应，其实给客户端最好的回复就是“不回复”，这样可以减轻服务端的压力。
+         * 假如有10w个空闲Idle的连接，那么服务端光响应心跳，都需要消耗大量资源！
+         * 那么怎么才能告诉客户端它还活着呢，其实很简单，因为5s服务端都会收到来自客户端的心跳信息，
+         * 如果10秒，甚至15秒内都收不到客户端的心跳，服务端可以认为客户端挂了，可以close链路
+         */
         ctx.channel().writeAndFlush(HB_ACK);
     }
 
