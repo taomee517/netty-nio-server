6c3cc0c765c25b25be038465ed947d0642ee6ad4
taomee517 <taomee517@qq.com>
2019-09-21 17:44:32 +0800

    完善DTU模拟设备

--- src/main/java/com/demo/netty/fakedevice/kt20/handler/DeviceEncodeHandler.java
+++ src/main/java/com/demo/netty/fakedevice/kt20/handler/DeviceEncodeHandler.java
@@ -15,32 +15,57 @@ import org.apache.commons.lang3.StringUtils;
  */
 @Slf4j
 public class DeviceEncodeHandler extends ChannelInboundHandlerAdapter {
+    private static final String SIGN_RESP_PREFIX = "7E8100";
+    private static final String HEART_BEAT_PREFIX = "7E80010005";
+    private static final String TRANSMIT_MSG_PREFIX = "7E8900";
+    private static final String CONTROL_CMD_SIGN = "372362353031";
+    private static final String QUERY_VERSION_SIGN = "312362313031";
     private int index;
     private int shardIndex;
     private int shardValidIndex;
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String target = null;
+        String resp = null;
         String serverMsg = msg.toString();
-        if(serverMsg.contains("7E80010810")){
+
+        if(serverMsg.contains(SIGN_RESP_PREFIX)){
+            log.info("服务器登录回复：{}",serverMsg);
+            serverMsg = null;
+        }else if(serverMsg.contains(HEART_BEAT_PREFIX)){
             log.info("心跳 ↓");
             serverMsg = null;
-        }else if (index<1) {
+        }
+
+        if (index==0) {
+            //第一条消息，发送一个定位
             Thread.sleep(4000);
             String up = "7E02000840014533224352000497089B87B32C2629B87D02AA57D700BFC1DBB1C5A8A7C7CB4B5B59859FDB6622B8AB9B351C3C7F316235FC8AD2458FDC52F1E047447FA8879F9B0DE948041820D48D7E";
             log.info("设备上报定位消息： {}", up);
             ctx.channel().writeAndFlush(up);
             index ++;
-        }else if (serverMsg.contains("7E8900")){
-            log.info("透传消息：{}",serverMsg);
-            serverMsg = null;
-//            String target = "查询b101";
-//            String resp = "7E09000860014533?22435200081D08941CA96C42BD24321F704B27C82A75F040925EE2B3D35F5B6EA441E091405FCD7BD0D0572624AE7A21E274AF9E35D1B9D66BC7A692F52183AEE8ADAB2B6ECEA81C684E9945900FA7AD2271E0657A7D8A220E4FA8927A4664B6AB17A1350C3A7E";
-            String target = "控制-解锁";
-            String resp = "7E0900000E014533224352037B41362C3723623430312C322C3223007E";
-            log.info("设备回复 {} 结果：{}",target, resp);
-            ctx.channel().writeAndFlush(resp);
         }
+
+        if (StringUtils.isNotEmpty(serverMsg)) {
+            if (serverMsg.contains(TRANSMIT_MSG_PREFIX) && serverMsg.contains(CONTROL_CMD_SIGN)){
+                target = "控制-解锁";
+                log.info("透传消息,{}：{}",target,serverMsg);
+                serverMsg = null;
+                resp = "7E0900000E014533224352037B41362C3723623430312C322C3223007E";
+                log.info("设备回复 {} 结果：{}",target, resp);
+                ctx.channel().writeAndFlush(resp);
+            }else if (serverMsg.contains(TRANSMIT_MSG_PREFIX) && serverMsg.contains(QUERY_VERSION_SIGN)){
+                target = "查询b101";
+                log.info("透传消息,{}：{}",target,serverMsg);
+                serverMsg = null;
+                resp = "7E0900086001453322435200081D08941CA96C42BD24321F704B27C82A75F040925EE2B3D35F5B6EA441E091405FCD7BD0D0572624AE7A21E274AF9E35D1B9D66BC7A692F52183AEE8ADAB2B6ECEA81C684E9945900FA7AD2271E0657A7D8A220E4FA8927A4664B6AB17A1350C3A7E";
+                log.info("设备回复 {} 结果：{}",target, resp);
+                ctx.channel().writeAndFlush(resp);
+            }
+        }
+
+        //其他类型的服务器消息
         if (StringUtils.isNotEmpty(serverMsg)) {
             log.info("服务器消息: {}", serverMsg);
         }
